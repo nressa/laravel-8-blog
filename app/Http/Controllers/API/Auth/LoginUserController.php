a2dc0149659179ca02f5e92b0adc1c3f5be302ae
nressa <mressanova@gmail.com>
2023-03-18 20:04:07 +0800

    feat: create api for login

--- app/Http/Controllers/API/Auth/LoginUserController.php
+++ app/Http/Controllers/API/Auth/LoginUserController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\API\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\Auth\LoginRequest;
 
 class LoginUserController extends Controller
 {
     
-    public function submit()
-    {
-        dd(123);
+    public function submit(LoginRequest $request)
+    {   
+        return [
+            'status' => 200
+        ];
     }
 }
