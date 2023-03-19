78a4bf959f690be36755e6a6960e5773d7df019f
nressa <mressanova@gmail.com>
2023-03-19 22:13:53 +0800

    feat:add main layout - Add layout for list page - Add layout for blog detail page

---
+++ resources/views/layouts/main_app.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Free Web tutorials">
+        <meta name="keywords" content="HTML, CSS, JavaScript">
+        <meta name="author" content="John Doe">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Laravel') }} @yield('title')</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/blog.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <header>
+            @yield('banner')
+        </header>
+        @yield('content')
+
+        <footer class="text-center">
+            Copyright@2023
+        </footer>
+    </body>
+</html>
