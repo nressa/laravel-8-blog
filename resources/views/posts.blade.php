dbc8ce047e865ec0a19b0364922cc15881ca522c
nressa <mressanova@gmail.com>
2023-03-27 03:59:19 +0800

    feat:Episode 31 - Convert the HTML and CSS to Blade - Get the asset from the github Laravel-From-Scratch-HTML-CSS - Apply the component layout and partial layout from the laravel

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,18 @@
-@extends('layouts.main_app')
-@section('banner')
-    <h1 class="text-center">My Post-it</h1>
-@endsection
-@section('content')
-    <div class="body">
-        @foreach ($posts as $post)
-        <article class="{{ $loop->even ? 'bg-active' : '' }}">
-            <h2>
-                <a href="/posts/{{ $post['slug'] }}">
-                    {!! $post->title !!}
-                </a>
-            </h2>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> 
-                in <a href="/categories/{{ $post->category['slug'] }}">{{ $post->category->name }}</a>
-            </p>
-            {{ $post->excerpt }}
-        </article>
-        @endforeach
+<x-layout>
+@include('sections._posts-header')
+
+<main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card />
+
+    <div class="lg:grid lg:grid-cols-2">
+        <x-post-card /> 
+        <x-post-card />
     </div>
-@endsection
+
+    <div class="lg:grid lg:grid-cols-3">
+        <x-post-card />
+        <x-post-card />
+        <x-post-card />
+    </div>
+</main>
+</x-layout>
